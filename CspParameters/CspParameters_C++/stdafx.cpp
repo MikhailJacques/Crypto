5a72baf1b0c07f7e5e4b02b82e86774bfeacadda
Mikhail Jacques <jacques.mikhail@gmail.com>
2014-10-17 13:52:57 +0300

    update projects

--- CspParameters/CspParameters_C++/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// CspParameters.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
